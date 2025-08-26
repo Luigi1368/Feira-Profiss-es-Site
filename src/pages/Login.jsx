2bd71e5bd9d64678d521525df10020eb7f685167
Luigi <banzattoluigis@gmail.com>
2025-08-26 10:23:03 -0300

    tirar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,10 @@
-import { Link } from "react-router";
-import { useEffect } from "react";
-import slides from './scipts.js'
 import "./Login.scss";
 
 export default function Login() {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      const startSlider = slides();
-      startSlider();
-    }, 100);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
 
-    <header className='container-cabeçalho'></header>
+      <header className='container-cabeçalho'></header>
         <menu className='container-principal'>
             <section className='container-dentro'>
                     <h1>Login</h1>
